2ef6745bb1dce6ee23ccaa09bb8e8f0a36cb29d2
RajDev12 <rjnath98@gmail.com>
2024-05-15 12:16:46 +0530

    KNN implementation

--- KNN.R
+++ KNN.R
@@ -16,7 +16,7 @@ wbcd_train <- wbcd_n[1:469, ]
 wbcd_test <- wbcd_n[470:569, ]
 wbcd_train_labels <- wbcd[1:469, 1]
 wbcd_test_labels <- wbcd[470:569, 1]
-install.packages("class")
+# install.packages("class")
 library(class)
 wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test,
                       cl = wbcd_train_labels, k = 21)
